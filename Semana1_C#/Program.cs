fee306757bc74014f1af7a50f3c43a621892ee28
FAbio <fabiobenavides086@gmail.com>
2025-09-02 15:32:49 -0500

    subiendo ejercicio-semanal1

---
+++ Semana1_C#/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+
+        }
+        static void ejer1()
+        {
+            string nombre, carrera; //declarando variables
+            Console.WriteLine("ingrese su nomnbre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese su carrera: ");
+            carrera = Console.ReadLine();
+            Console.WriteLine($"\n{nombre}, bienvenido a FA de {carrera}");
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3() 
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+
+
+}
