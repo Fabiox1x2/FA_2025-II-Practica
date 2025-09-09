9ff6141c7f3d75da8dd38838f53802c695c975b6
FAbio <fabiobenavides086@gmail.com>
2025-09-09 14:53:56 -0500

    ejer2 sem3

--- Semana1_Python/Semana1_Python.py
+++ Semana1_Python/Semana1_Python.py
@@ -4,4 +4,13 @@ def ejer1():#creando metodo ejer1
 
     print(f"\n{nombre}, bienvenido a FA de {carrera}")
 
-ejer1()
+def ejer2():
+    x= int(input("ingrese el valor de x: "))
+    y= int(input("Ingrese el valor de y: "))
+
+    print("Suma: ", (x+y))
+    print("Resta: ",(x-y))
+    print("Multiplicacion: ", (x*y))
+    print("Division: ", (x/y))
+
+ejer2()
